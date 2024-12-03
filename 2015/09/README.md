3e2c405e9b16eb65b99bc94b4bdde0bc3c91bba7
Chris Grau <113591+sirhc@users.noreply.github.com>
2024-12-03 15:39:44 -0800

    Solution for 2015, day 9, part 1

---
+++ 2015/09/README.md
@@ -0,0 +1,21 @@
+# Day 9: All in a Single Night
+
+https://adventofcode.com/2015/day/9
+
+## Part One
+
+```
+❯ perl distance.pl input | tail
+AlphaCentauri -> Arbre -> Faerun -> Norrath -> Snowdin -> Tambi -> Straylight -> Tristram = 526
+Tristram -> AlphaCentauri -> Arbre -> Faerun -> Norrath -> Snowdin -> Straylight -> Tambi = 631
+AlphaCentauri -> Tristram -> Arbre -> Faerun -> Norrath -> Snowdin -> Straylight -> Tambi = 637
+AlphaCentauri -> Arbre -> Tristram -> Faerun -> Norrath -> Snowdin -> Straylight -> Tambi = 669
+AlphaCentauri -> Arbre -> Faerun -> Tristram -> Norrath -> Snowdin -> Straylight -> Tambi = 584
+AlphaCentauri -> Arbre -> Faerun -> Norrath -> Tristram -> Snowdin -> Straylight -> Tambi = 683
+AlphaCentauri -> Arbre -> Faerun -> Norrath -> Snowdin -> Tristram -> Straylight -> Tambi = 614
+AlphaCentauri -> Arbre -> Faerun -> Norrath -> Snowdin -> Straylight -> Tristram -> Tambi = 589
+AlphaCentauri -> Arbre -> Faerun -> Norrath -> Snowdin -> Straylight -> Tambi -> Tristram = 562
+207
+```
+
+## Part Two
---
+++ 2015/09/distance.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use v5.40;
+use strict;
+
+use Algorithm::Permute;
+
+my %distance;
+my $shortest;
+
+while ( my $line = <> ) {
+  chomp $line;
+
+  # X to Y = ###
+  $line =~ m/^(\S+) to (\S+) = (\d+)$/;
+
+  # Store the distance for both locations for easy lookup.
+  $distance{$1}{$2} = $distance{$2}{$1} = $3;
+}
+
+# use Data::Dump;
+# dd \%distance;
+
+# 40,320 permutations!!
+my $p = Algorithm::Permute->new( [ sort keys %distance ] );
+
+sub distance {
+  my ( @locations ) = @_;
+
+  my $current_location = shift @locations;
+  my $distance = 0;
+
+  while ( @locations > 0 ) {
+    # say $current_location, ' -> ', $locations[0], ' = ', $distance{$current_location}{ $locations[0] };
+    $distance += $distance{$current_location}{ $locations[0] };
+    $current_location = shift @locations;
+  }
+  # say $distance;
+
+  return $distance;
+}
+
+# Brute force, baby!
+while ( my @places = $p->next ) {
+  my $distance = distance( @places );
+  say sprintf '%s = %d', ( join ' -> ', @places ), $distance;
+
+  if ( !defined $shortest || $distance < $shortest ) {
+    $shortest = $distance;
+  }
+}
+
+say $shortest;
