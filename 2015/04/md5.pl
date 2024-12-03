785bc0b8db733f2a6367849aebd83218cb3eba89
Chris Grau <113591+sirhc@users.noreply.github.com>
2024-12-03 09:14:22 -0800

    Solution for 2015, day 4, part 2

--- 2015/04/md5.pl
+++ 2015/04/md5.pl
@@ -6,11 +6,12 @@ use strict;
 use Digest::MD5 qw( md5_hex );
 
 my $input = $ARGV[0];
+my $match = $ARGV[1] // '00000';
 my $n     = 0;
 
 while ( 1 ) {
   my $hash = md5_hex("$input$n");
   say $hash, ' ', $n;
-  last if $hash =~ /^00000/;
+  last if $hash =~ /^$match/;
   $n += 1;
 }
