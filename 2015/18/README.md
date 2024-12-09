41571acfcf4b3935f360e71b677bba0b168114f1
Chris Grau <113591+sirhc@users.noreply.github.com>
2024-12-08 21:13:25 -0800

    Solution for 2015, day 18

---
+++ 2015/18/README.md
@@ -0,0 +1,35 @@
+# Day 18: Like a GIF For Your Yard
+
+https://adventofcode.com/2015/day/18
+
+## Part One
+
+```
+❯ perl animate.pl 100 input | tail
+Iteration 91: 1120
+Iteration 92: 1082
+Iteration 93: 1096
+Iteration 94: 1080
+Iteration 95: 1043
+Iteration 96: 1087
+Iteration 97: 1051
+Iteration 98: 1079
+Iteration 99: 1016
+Iteration 100: 1061
+```
+
+## Part Two
+
+```
+❯ perl animate2.pl 100 input | tail
+Iteration 91: 1063
+Iteration 92: 1053
+Iteration 93: 1046
+Iteration 94: 1087
+Iteration 95: 1062
+Iteration 96: 1079
+Iteration 97: 1021
+Iteration 98: 1052
+Iteration 99: 996
+Iteration 100: 1006
+```
---
+++ 2015/18/animate.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+
+use v5.40;
+use strict;
+
+use Algorithm::Combinatorics qw( variations_with_repetition );
+use List::Util qw( sum );
+use Data::Dump;
+
+my $iterations = shift // 100;
+my $grid       = [];  # $grid->[$row][$col] = 1|0
+my $next       = [];
+
+while ( my $line = <> ) {
+  chomp $line;
+  push @{ $grid }, [ map { $_ eq '#' ? 1 : 0 } split //, $line ];
+}
+
+# dd $grid;
+
+sub neighbor {
+  my ( $grid, $r, $c ) = @_;
+
+  return 0 if $r < 0 || $r > $#{ $grid };
+  return 0 if $c < 0 || $c > $#{ $grid->[$r] };
+  # dd $r, $c, $grid->[$r][$c];
+  return $grid->[$r][$c];
+}
+
+my @neighbors = grep { !( $_->[0] == 0 && $_->[1] == 0 ) } variations_with_repetition( [ -1 .. 1 ], 2 );
+
+for my $iteration ( 0 .. $iterations ) {
+  # say for map { join '', map { $_ ? '#' : '.' } @$_ } @$grid;
+  # say '';
+  say "Iteration $iteration: ", sum map { sum @$_ } @$grid;
+  # say '';
+
+  for my $r ( 0 .. $#{ $grid } ) {
+    for my $c ( 0 .. $#{ $grid->[$r] } ) {
+      my $sum = sum map { neighbor( $grid, $r + $_->[0], $c + $_->[1]) } @neighbors;
+
+      # dd $r, $c, '->', $grid->[$r][$c], $sum;
+
+      $next->[$r][$c] = $grid->[$r][$c];
+
+      if ( $grid->[$r][$c] == 1 ) {
+        $next->[$r][$c] = $sum == 2 || $sum == 3 ? 1 : 0;
+        next;
+      }
+
+      if ( $grid->[$r][$c] == 0 ) {
+        $next->[$r][$c] = $sum == 3 ? 1 : 0;
+        next;
+      }
+    }
+  }
+
+  for my $r ( 0 .. $#{ $grid } ) {
+    for my $c ( 0 .. $#{ $grid->[$r] } ) {
+      $grid->[$r][$c] = $next->[$r][$c];
+    }
+  }
+}
