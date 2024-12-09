e50aec6d158434ce73cd5150398c50a56a157a22
Chris Grau <113591+sirhc@users.noreply.github.com>
2024-12-08 17:20:53 -0800

    Solution for 2015, day 15, part 1

---
+++ 2015/15/README.md
@@ -0,0 +1,19 @@
+# Day 15: Science for Hungry People
+
+https://adventofcode.com/2015/day/15
+
+## Part One
+
+```
+❯ perl start.pl example | sort -n | tail -1
+62842880
+
+❯ time perl start.pl input | sort -n | tail -1
+18965440
+perl start.pl input  1.89s user 0.02s system 98% cpu 1.927 total 49k max rss
+```
+
+## Part Two
+
+```
+```
---
+++ 2015/15/score.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+use v5.40;
+use strict;
+
+use Data::Dump;
+use List::Util qw( reduce sum );
+use List::MoreUtils qw( pairwise );
+
+my %ingredient;
+
+while ( my $line = <> ) {
+  chomp $line;
+
+  # Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+  $line =~ m/^(\w+): (.*)/;
+
+  $ingredient{$1}{ $_->[0] } = $_->[1] for map { [ split ] } split /, /, $2;
+}
+
+# dd \%ingredient;
+
+# Generate all possible combinations of numbers that add to 100.
+sub make_list {
+  my ( $sum, $size, $list ) = @_;
+
+  if ( $size == 1 ) {
+    return [ @$list, $sum ];
+  }
+
+  map { make_list( $sum - $_, $size - 1, [ @$list, $_ ] ) } ( 0 .. $sum );
+}
+
+for my $combination ( make_list( 100, ( scalar keys %ingredient ), [] ) ) {
+  my %score = ();
+
+  # say join ', ', @$combination;
+
+  for my $property ( qw( capacity durability flavor texture ) ) {
+    $score{$property} = sum pairwise { $a * $ingredient{$b}{$property} } @$combination, @{[ sort keys %ingredient ]};
+  }
+
+  # dd \%score;
+
+  say reduce { $a * $b } map { $_ < 0 ? 0 : $_ } values %score;
+}
