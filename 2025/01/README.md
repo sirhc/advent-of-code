299561261328771b544296caf4888be0647e5e18
Chris Grau <113591+sirhc@users.noreply.github.com>
2025-12-01 16:17:36 -0800

    Add my solution for part two
    I kept getting the wrong answer because I was never able to properly
    account for the edge cases when trying to do the math. I finally gave up
    and wrote a stupid loop. It was slower, but it worked the first time.

--- 2025/01/README.md
+++ 2025/01/README.md
@@ -24,5 +24,29 @@ Password: 3
 
 ## Part Two
 
+Part one was fairly straightforward in that I only needed to determine if the final position was a multiple of 100. However, now I need to determine how many times I
+pass zero.
+
+The magnitude of the turn will tell us how many times we pass zero. For every 100 clicks, we return to the starting position, which means we either passed zero or
+started and ended at zero. So, a turn of 150 clicks will pass zero at least once. Additionally, depending on the starting position, we may pass zero again or potentially
+end at zero.
+
+My shortcut in part one isn't going to work here. To use it, I would need to determine how many multiples of 100 lie between the start and end positions, which would
+necessitate a loop to account for crossing actual zero. And there would still be a special case for ending on a multiple of 100.
+
+Ugh. Finally, I gave up on figuring out all the edge cases to deal with the math and wrote something that looped through each click of the turn, counting how many times
+we hit zero. It ran longer, but still finished in a couple of seconds. I left my math code in, just in case I want to revisit it later.
+
 ```
+❯ zsh pass.zsh 2 < input | tail
+21 + L40  -> 81 (6636)
+81 + L1   -> 80 (6636)
+80 + R18  -> 98 (6636)
+98 + R7   ->  5 (6637)
+ 5 + R2   ->  7 (6637)
+ 7 + R48  -> 55 (6637)
+55 + R31  -> 86 (6637)
+86 + R25  -> 11 (6638)
+
+Password for Part Two: 6638
 ```
--- 2025/01/pass.zsh
+++ 2025/01/pass.zsh
@@ -1,21 +1,114 @@
 #!/usr/bin/env zsh
 
-dial=50
-pass=0
+set -eu -o pipefail
 
-while read -r line; do
-  case $line in
-    L*)
-      printf '%4d %-3s -> %4d\n' $dial $line $(( dial -= ${line#L} ))
-      ;;
-    R*)
-      printf '%4d %-3s -> %4d\n' $dial $line $(( dial += ${line#R} ))
-      ;;
-  esac
+part_one() {
+  local dial=50
+  local pass=0
+  local line
 
-  if (( dial % 100 == 0 )); then
-    (( pass++ ))
-  fi
-done
+  while read -r line; do
+    case $line in
+      L*)
+        printf '%4d %-3s -> %4d\n' $dial $line $(( dial -= ${line#L} ))
+        ;;
+      R*)
+        printf '%4d %-3s -> %4d\n' $dial $line $(( dial += ${line#R} ))
+        ;;
+    esac
 
-printf '\nPassword: %d\n' $pass
+    if (( dial % 100 == 0 )); then
+      pass=$(( pass + 1 ))
+    fi
+  done
+
+  printf '\nPassword for Part One: %d\n' $pass
+}
+
+part_two() {
+  local dial=50
+  local pass=0
+  local line
+  local init
+
+  printf '%-4s -> %2d (%4d)\n' S $dial $pass
+
+  while read -r line; do
+    init=$dial
+    last=$pass
+
+    case $line in
+      L*)
+        if (( ${line#L} >= 100 )); then
+          pass=$(( pass + ${line#L} / 100 - 1 - (init == 0 ? 1 : 0) ))  # for every 100 clicks, we're guaranteed to pass zero (starting on zero means one fewer passes)
+        fi
+        dial=$(( dial - ${line#L} ))
+        ;;
+      R*)
+        if (( ${line#R} >= 100 )); then
+          pass=$(( pass + ${line#R} / 100 - 1 - (init == 0 ? 1 : 0) ))
+        fi
+        dial=$(( dial + ${line#R} ))
+        ;;
+    esac
+
+    # If we've gone negative, we passed zero, unless we started at zero. Multiple revolutions are handled above.
+    if (( dial < 0 && init != 0 )); then
+      pass=$(( pass + 1 ))
+    fi
+
+    # If we've gone past 99, we passed zero. Unless our landing position is on zero, which we count later.
+    if (( dial > 99 && dial % 100 != 0 )); then
+      pass=$(( pass + 1 ))
+    fi
+
+    # Set the dial to the real position (e.g., -10 is actually 90).
+    dial=$(( ( dial % 100 + 100 ) % 100 ))
+
+    # We also need to count when we've landed on zero.
+    if (( dial == 0 )); then
+      pass=$(( pass + 1 ))
+    fi
+
+    if (( last != pass )); then
+      printf '%2d + %-4s -> %2d (%4d)\n' $init $line $dial $pass
+    fi
+  done
+
+  printf '\nPassword for Part Two: %d\n' $pass
+}
+
+part_two_loop() {
+  local dial=50 init
+  local pass=0
+  local line direction clicks click
+
+  while read -r line; do
+    init=$dial
+    direction=${line:0:1}
+    clicks=${line:1}
+
+    case $direction in
+      L ) direction=-1 ;;
+      R ) direction=1  ;;
+    esac
+
+    for (( click = 0; click < clicks; click++ )); do
+      dial=$(( ( ( dial + direction ) % 100 + 100 ) % 100 ))
+      pass=$(( pass + ( dial == 0 ? 1 : 0 ) ))
+    done
+
+    printf '%2d + %-4s -> %2d (%4d)\n' $init $line $dial $pass
+  done
+
+  printf '\nPassword for Part Two: %d\n' $pass
+}
+
+case "${1-one}" in
+  1 | one | part1 | part_one )
+    part_one
+    ;;
+  2 | two | part2 | part_two )
+    part_two_loop
+    ;;
+esac
