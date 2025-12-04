2012d45b2c37b4ae9bdfa4b63fd0b970200a8344
Chris Grau <113591+sirhc@users.noreply.github.com>
2025-12-04 15:17:06 -0800

    Updated solution to account for the additional complexity of part two

--- 2016/11/README.md
+++ 2016/11/README.md
@@ -32,11 +32,49 @@ perl rtg.pl < input  27.63s user 0.12s system 99% cpu 27.902 total 154k max rss
 
 ## Part Two
 
-I suppose I should have seen this coming. How do you mess with people who wrote a breadth-first search? Throw extra states at them. Of course, the additional isotopes aren't real elements, so my
-fancy rendering function needs additional data.
+I suppose I should have seen this coming. How do you mess with people who wrote a breadth-first search? Throw extra states at them. Of course, the additional isotopes
+aren't real elements, so my fancy rendering function needed additional data.
 
-As I prepare for running part two, I ran part one on a MacBook Pro M1, instead of my Linux Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz:
+As I prepared for running part two, I ran part one on a MacBook Pro M1, instead of my Linux Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz:
 
 ```
 perl rtg.pl < input  15.67s user 0.15s system 98% cpu 16.029 total 150272k max rss
 ```
+
+It turned out that the amount of time part two was taking frustrated me. I looked over the Reddit discussion to see where I could optimize my code, looking for ways I
+could more aggressively prune the search space. I didn't have much luck until I ran across this comment:
+
+> Any complete pairs on floor 1 add 12 steps to the solution
+> Complete pairs on floor 2 add 8 steps
+> Complete pairs on floor 3 add 4 steps
+>
+> https://www.reddit.com/r/adventofcode/comments/5hoia9/comment/db1v3ar/
+
+Based on my initial thoughts about the problem, knowing there are formulae that can be used to determine the number of moves for problems like the Towers of Hanoi or the
+jealous husbands problem, I realized this made sense and gave it a shot.
+
+```
+❯ perl rtg.pl < input2
+Initial state:
+
+F4  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .
+F3  .   .   .   .   .   PmG PmM .   .   RuG RuM .   .   .   .
+F2  .   .   .   .   .   .   .   .   PuM .   .   .   SrM .   .
+F1  E   DiG DiM ElG ElM .   .   PuG .   .   .   SrG .   TmG TmM
+
+Known moves: 44
+
+F4  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .
+F3  .   .   .   .   .   .   .   .   .   .   .   .   .   .   .
+F2  .   .   .   .   .   .   .   .   PuM .   .   .   SrM .   .
+F1  E   .   .   .   .   .   .   PuG .   .   .   SrG .   .   .
+
+Iteration:     355 : Depth:  11 : Queue:  44
+
+Minimum number of moves: 55
+```
+
+Of course, I think I spent more time modifying and debugging the way I maintained state than it would have taken to just let my original breadth-first search run its
+course.
+
+¯\_(ツ)_/¯
--- 2016/11/rtg.pl
+++ 2016/11/rtg.pl
@@ -3,14 +3,17 @@
 use v5.40;
 use strict;
 use Math::Combinatorics qw( combine );
-use Storable qw( dclone );
+use Clone qw( clone );
+use List::Util qw( any pairs );
+
+use Data::Dump;
 
 my %elements = map { split } <DATA>;  # these are just for visualization purposes
 my %isotopes = ();
 
 my $state = {
   elevator => 0,
-  floors   => [ [], [], [], [] ],
+  floors   => [ {}, {}, {}, {} ],  # [ { HeG => 1, HeM => 0 }, ... ]
 };
 
 while ( defined ( my $line = <> ) ) {
@@ -26,16 +29,55 @@ while ( defined ( my $line = <> ) ) {
 
     $isotopes{ $elements{$element} } = 1;
 
-    push @{ $state->{'floors'}[$floor] }, $elements{$element} . ( $type eq 'generator' ? 'G' : 'M' );
+    #push @{ $state->{'floors'}[$floor] }, $elements{$element} . ( $type eq 'generator' ? 'G' : 'M' );
+    $state->{'floors'}[$floor]{ $elements{$element} . ( $type eq 'generator' ? 'G' : 'M' ) } = 1;
   }
 }
 
 my $iterations = 0;
+my $known      = 0;
 
-printf "\e[?25l";  # hide the cursor
+say 'Initial state:';
+say '';
+say render($state);
+# say serialize($state);
+say '';
+
+# say is_safe( $state->{'floors'}[0] ) ? 'Floor 1 is safe' : 'Floor 1 is NOT safe';
+# say is_safe( $state->{'floors'}[1] ) ? 'Floor 2 is safe' : 'Floor 2 is NOT safe';
+# say is_safe( $state->{'floors'}[2] ) ? 'Floor 3 is safe' : 'Floor 3 is NOT safe';
+# say is_safe( $state->{'floors'}[3] ) ? 'Floor 4 is safe' : 'Floor 4 is NOT safe';
+# exit;
+
+# dd $state;
+# exit;
+
+for my $floor ( 0 .. 3 ) {
+  for my $element ( keys %elements ) {
+    my $generator = $elements{$element} . 'G';
+    my $microchip = $elements{$element} . 'M';
+
+    if ( exists $state->{'floors'}[$floor]{$generator} && exists $state->{'floors'}[$floor]{$microchip} ) {
+      $state->{'floors'}[$floor]{$generator} = 0;
+      $state->{'floors'}[$floor]{$microchip} = 0;
+
+      $known += 12 - 4 * $floor;
+    }
+  }
+
+  # $state->{'floors'}[$floor] = [ map { $_->[0] } grep { $_->[1] == 1 } pairs %{ $state->{'floors'}[$floor] } ];
+}
+
+# dd [ $known, $state ];
+
+# exit;
+
+say 'Known moves: ', $known;
+say '';
 say render($state);
 say '';
-say 'Minimum number of moves: ', bfs($state);
+printf "\e[?25l";  # hide the cursor
+say 'Minimum number of moves: ', bfs($state) + $known;
 printf "\e[?25h";  # show the cursor
 
 sub bfs {
@@ -47,7 +89,7 @@ sub bfs {
     my ( $state, $depth ) = @{ shift @queue };
 
     # Skip this state if we've already seen it.
-    next if $visited{ join '|', $state->{elevator}, map { join ',', sort @$_ } @{ $state->{floors} } }++;  # e.g., '0|HM,LiM|HG|LiG|'
+    next if $visited{ serialize($state) }++;
       
     # Return the depth of the search if the goal state is reached.
     if ( is_goal($state) ) {
@@ -64,11 +106,23 @@ sub bfs {
   return -1;  # no solution found
 }
 
+sub get_items {
+  my ( $state, $floor ) = @_;
+
+  return map { $_->[0] } grep { $_->[1] == 1 } pairs %{ $state->{'floors'}[$floor] };
+}
+
+sub serialize {
+  my ( $state ) = @_;
+
+  return join '|', $state->{'elevator'}, map { my $floor = $_; join ',', sort +get_items( $state, $floor ) } 0 .. 3;  # e.g., '0|HM,LiM|HG|LiG|'
+}
+
 sub is_goal {
   my ( $state ) = @_;
 
   for my $floor ( 0 .. 2 ) {
-    return 0 if @{ $state->{floors}[$floor] };  # one of floors 1–3 is not empty
+    return if any { $_ == 1 } values %{ $state->{'floors'}[$floor] };  # one of floors 1–3 is not empty
   }
 
   return 1;
@@ -79,26 +133,36 @@ sub generate_moves {
 
   # Generate a list of all possible moves from the current floor.
   my $current_floor = $state->{'elevator'};
-  my @items = @{ $state->{'floors'}[$current_floor] };
+  my @items = get_items( $state, $current_floor );
   my @moves = ( combine( 1, @items ), combine( 2, @items ) );
 
   my @new_states;
 
+    # Prioritize taking two items up and one item down.
+    # for my $move ( sort { $direction == 1 ? @$b <=> @$a : @$a <=> @$b } @moves ) {
   for my $move ( @moves ) {
     for my $direction ( -1, 1 ) {  # down, up
       my $new_floor = $current_floor + $direction;
       next if $new_floor < 0 || $new_floor > 3;  # out of bounds
 
+      # next if $direction ==  1 && @$move == 1;   # don't take a single item up
+      # next if $direction == -1 && @$move == 2;   # don't take two items down
+
+      # if ( $direction == -1 && @{ $state->{'floors'}[$new_floor] } == 0 ) {
+      #   next if @$move == 1;  # don't take a single item down to an empty floor
+      #   next if ( $move->[0] =~ s/.$//r ) eq ( $move->[1] =~ s/.$//r );  # don't take two items of the same type down
+      # }
+
       # Set up the new state.
-      my $new_state = dclone($state);
+      my $new_state = clone($state);
 
       $new_state->{'elevator'} = $new_floor;
 
       # Remove items from the current floor.
-      $new_state->{'floors'}[$current_floor] = [ grep { my $item = $_; !grep { $_ eq $item } @$move } @{ $state->{'floors'}[$current_floor] } ];
+      $new_state->{'floors'}[$current_floor]{$_} = 0 for @$move;
 
       # Add items to the new floor.
-      push @{ $new_state->{'floors'}[$new_floor]}, @$move;
+      $new_state->{'floors'}[$new_floor]{$_} = 1 for @$move;
 
       next unless is_safe( $new_state->{'floors'}[$current_floor] );  # ensure we left the current floor safe
       next unless is_safe( $new_state->{'floors'}[$new_floor] );      # ensure the new floor is safe
@@ -113,13 +177,11 @@ sub generate_moves {
 sub is_safe {
   my ( $floor ) = @_;
 
-  for my $item ( @$floor ) {
-    next if $item =~ /G$/;                 # generators aren't affected by other items
-
-    my $generator = $item =~ s/M$/G/r;
-    next if grep { $_ eq $generator } @$floor;  # the microchip's paired generator is present
-
-    return if grep { /G$/ } @$floor;     # the microchip is fried if any other generator is present
+  # A floor is represented by a hash: { HeG => 1, HeM => 0, LiG => 1, LiM => 1 }.
+  for my $item ( grep { /M$/ } keys %$floor ) {
+    next if $floor->{ $item } == 0;              # the microchip is not actually present (due to state representation)
+    next if $floor->{ $item =~ s/M$/G/r } // 0;  # the microchip's paired generator is present
+    return if grep { $_->[0] =~ /G$/ && $_->[1] == 1 } pairs %$floor;  # the microchip is fried if any other generator is present
   }
 
   return 1;
@@ -134,8 +196,11 @@ sub render {
     push @{ $map[ $floor ] }, sprintf '%-3s', $state->{'elevator'} == $floor ? 'E' : '.';
 
     for my $isotope ( sort keys %isotopes ) {
-      push @{ $map[ $floor ] }, sprintf '%-3s', ( grep { $_ eq "${isotope}G" } @{ $state->{'floors'}[$floor] } ) ? "${isotope}G" : '.';
-      push @{ $map[ $floor ] }, sprintf '%-3s', ( grep { $_ eq "${isotope}M" } @{ $state->{'floors'}[$floor] } ) ? "${isotope}M" : '.';
+      for my $item ( "${isotope}G", "${isotope}M" ) {
+        push @{ $map[ $floor ] }, sprintf '%-3s', $state->{'floors'}[$floor]{$item} // 0 > 0 ? $item : '.';
+      }
+      # push @{ $map[ $floor ] }, sprintf '%-3s', ( grep { $_ eq "${isotope}G" } @{ $state->{'floors'}[$floor] } ) ? "${isotope}G" : '.';
+      # push @{ $map[ $floor ] }, sprintf '%-3s', ( grep { $_ eq "${isotope}M" } @{ $state->{'floors'}[$floor] } ) ? "${isotope}M" : '.';
     }
   }
 
