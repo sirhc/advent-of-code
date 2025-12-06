97274b56320260ea0e9d3af8a153c4b2b7b65d75
Chris Grau <113591+sirhc@users.noreply.github.com>
2025-12-05 19:04:53 -0800

    Clean up a bit before part two

--- 2016/17/vault.pl
+++ 2016/17/vault.pl
@@ -3,18 +3,16 @@
 use v5.40;
 use strict;
 use Digest::MD5 qw( md5_hex );
-no Smart::Comments;
+use Smart::Comments;
 
 my $input = shift // die 'missing input';
-my $start = [ 0, 0 ];
-my $vault = [ 3, 3 ];
 
-my %moves = (       # +-----------------+
-  U => [  0, -1 ],  # | 0,0 1,0 2,0 3,0 |
-  D => [  0,  1 ],  # | 0,1 1,1 2,1 3,1 |
-  L => [ -1,  0 ],  # | 0,2 1,2 2,2 3,2 |
-  R => [  1,  0 ],  # | 0,3 1,3 2,3 3,3 |
-);                  # +-----------------+
+my %moves = (                                      # +-----------------+
+  U => sub { [ $_[0]->[0],    $_[0]->[1] - 1 ] },  # | 0,0 1,0 2,0 3,0 |
+  D => sub { [ $_[0]->[0],    $_[0]->[1] + 1 ] },  # | 0,1 1,1 2,1 3,1 |
+  L => sub { [ $_[0]->[0] -1, $_[0]->[1]     ] },  # | 0,2 1,2 2,2 3,2 |
+  R => sub { [ $_[0]->[0] +1, $_[0]->[1]     ] },  # | 0,3 1,3 2,3 3,3 |
+);                                                 # +-----------------+
 
 ### assert: wander('hijkl') eq ''
 ### assert: wander('ihgpwlah') eq 'DDRRRD'
@@ -26,12 +24,12 @@ say wander($input);
 sub wander {
   my ( $input ) = @_;
 
-  my @queue = ( [ $start, '' ] );
+  my @queue = ( [ [ 0, 0 ], '' ] );
 
   while ( @queue ) {
     my ( $position, $path ) = @{ shift @queue };
 
-    if ( $position->[0] == $vault->[0] && $position->[1] == $vault->[1] ) {
+    if ( $position->[0] == 3 && $position->[1] == 3 ) {
       return $path;
     }
 
@@ -41,15 +39,10 @@ sub wander {
     for my $pair ( [ 'U', $d[0] ], [ 'D', $d[1] ], [ 'L', $d[2] ], [ 'R', $d[3] ] ) {
       my ( $dir, $open ) = @$pair;
 
-      my $next_position = [ $position->[0] + $moves{$dir}->[0], $position->[1] + $moves{$dir}->[1] ];
+      my $next = $moves{$dir}->( $position ); # [ $position->[0] + $moves{$dir}->[0], $position->[1] + $moves{$dir}->[1] ];
+      next unless $open && $next->[0] >= 0 && $next->[0] <= 3 && $next->[1] >= 0 && $next->[1] <= 3;
 
-      next unless $open
-        && $next_position->[0] >= 0
-        && $next_position->[0] <= $vault->[0]
-        && $next_position->[1] >= 0
-        && $next_position->[1] <= $vault->[1];
-
-      push @queue, [ $next_position, $path . $dir ];
+      push @queue, [ $next, $path . $dir ];
     }
   }
 
