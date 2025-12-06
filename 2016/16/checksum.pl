da20ed2ae689b99c3a24ab657b851a59d41cac57
Chris Grau <113591+sirhc@users.noreply.github.com>
2025-12-05 17:59:42 -0800

    Optimize for part two

--- 2016/16/checksum.pl
+++ 2016/16/checksum.pl
@@ -2,6 +2,7 @@
 
 use v5.40;
 use strict;
+use List::Util qw( pairmap );
 use Smart::Comments;
 
 my $length = shift // die 'missing length';
@@ -13,14 +14,18 @@ my $input  = shift // die 'missing input';
 ### assert: dragon_curve('111100001010') eq '1111000010100101011110000'
 
 ### assert: checksum('110010110100') eq '100'
-### assert: checksum('10000011110010000111') eq '01100'
+### assert: checksum('110010110100') eq '100'
+
+### assert: checksum2('10000011110010000111') eq '01100'
+### assert: checksum2('10000011110010000111') eq '01100'
 
 while ( length $input < $length ) {
   $input = dragon_curve($input);
 }
 
 say substr $input, 0, $length;
-say checksum( substr $input, 0, $length );
+# say checksum( substr $input, 0, $length );  # oh, you got me and my suboptimal solution, part two
+say checksum2( substr $input, 0, $length );
 
 sub dragon_curve {
   my ( $data ) = @_;
@@ -37,4 +42,16 @@ sub checksum {
 
   return $sum if length($sum) % 2 == 1;
   return checksum($sum);
+}
+
+sub checksum2 {
+  my ( $data ) = @_;
+
+  my @data = split //, $data;
+
+  while ( scalar @data % 2 == 0 ) {
+    @data = pairmap { $a eq $b ? '1' : '0' } @data;
+  }
+
+  return join '', @data;
 }
