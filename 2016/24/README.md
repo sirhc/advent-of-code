ea4634abd001373121db72b4f2b83093892d0d6f
Chris Grau <113591+sirhc@users.noreply.github.com>
2025-12-12 18:20:47 -0800

    Determine solution for part one

---
+++ 2016/24/README.md
@@ -0,0 +1,27 @@
+# Day 24: Air Duct Spelunking
+
+https://adventofcode.com/2016/day/24
+
+## Part One
+
+Oh boy, the [traveling salesman problem](https://en.wikipedia.org/wiki/Travelling_salesman_problem), but with a twist. We're in a maze and, according to the example,
+we're allowed to revisit locations. That last bit makes me wonder if I'll be able to benefit from trimming paths.
+
+My breadth-first search works well enough for the example. Unsurprisingly, not coming up with a way to prune paths caused my breadth-first search to run my laptop out of memory. Then I realized
+that there is a way to keep track of visited locations. A locations can be considered visited if we've been there before _and_ we've visited the same set of HVAC points. Doing that dropped my run
+time to under 3 seconds.
+
+That was a little too easy. Now I'm concerned about part two.
+
+```
+❯ perl hvac.pl < example
+14
+
+❯ perl hvac.pl < input
+490
+```
+
+## Part Two
+
+```
+```
---
+++ 2016/24/hvac.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+
+use v5.40;
+use strict;
+
+my $blueprint = [ map { chomp; [ split // ] } <> ];
+my @points    = find_points($blueprint);
+
+my @queue   = ( [ $points[0], [ 1, (0) x ( scalar @points - 1 ) ], 0 ] );  # [ [ location, points visited, distance traveled ], ... ]
+my %visited = ();                                                          # ( 'row,col,visited_0,visited_1,...' => count )
+
+while ( @queue ) {
+  my ( $location, $visited, $distance ) = @{ shift @queue };
+
+  if ( $blueprint->[ $location->[0] ][ $location->[1] ] =~ /^(\d)$/ ) {
+    $visited->[$1] = 1;
+  }
+
+  next if $visited{ join ',', @$location, @$visited }++;
+
+  if ( visited_all($visited) ) {
+    say $distance;
+    last;
+  }
+
+  push @queue, map { [ $_, [ @$visited ], $distance + 1 ] } neighbors( $blueprint, $location );
+}
+
+sub find_points {
+  my ( $blueprint ) = @_;
+
+  my @points = ();
+
+  for my $row ( 0 .. $#{ $blueprint } ) {
+    for my $col ( 0 .. $#{ $blueprint->[$row] } ) {
+      if ( $blueprint->[$row][$col] =~ /^(\d)$/ ) {
+        $points[$1] = [ $row, $col ];
+      }
+    }
+  }
+
+  return @points;
+}
+
+sub neighbors {
+  my ( $blueprint, $location ) = @_;
+
+  return grep { $blueprint->[ $_->[0] ][ $_->[1] ] ne '#' } map { [ $location->[0] + $_->[0], $location->[1] + $_->[1] ] } (
+    [ -1,  0 ],  # up
+    [  1,  0 ],  # down
+    [  0, -1 ],  # left
+    [  0,  1 ],  # right
+  );
+}
+
+sub visited_all {
+  my ( $visited ) = @_;
+
+  return ( grep { $_ == 0 } @$visited ) ? '' : 1;
+}
